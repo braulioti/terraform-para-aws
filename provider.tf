b83002c5f7572747ace41c903a146c9796be1e24
Bráulio Figueiredo <braulio@braulioti.com.br>
2026-06-29 14:17:12 -0300

    feat: remote backend

--- provider.tf
+++ provider.tf
@@ -5,6 +5,13 @@ terraform {
       version = "4.48.0"
     }
   }
+
+  backend "s3" {
+    bucket = "comunidade-devops-braulio"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
+  }
+
 }
 
 # Configure the AWS Provider
